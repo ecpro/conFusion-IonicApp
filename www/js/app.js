6f30e34ab76a8c4a4d707524a4078f778d5f38b9
Piyush Ravi <mails4ipiyush@gmail.com>
2016-12-26 17:00:33 +0530

    add MenuController, DishDetailController and populate ng templates

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('conFusion', ['ionic', 'conFusion.controllers'])
+angular.module('conFusion', ['ionic', 'conFusion.controllers','conFusion.services'])
 
 .run(function ($ionicPlatform) {
     $ionicPlatform.ready(function () {
@@ -36,7 +36,8 @@ angular.module('conFusion', ['ionic', 'conFusion.controllers'])
         url: '/home',
         views: {
             'mainContent': {
-                templateUrl: 'templates/home.html'
+                templateUrl: 'templates/home.html',
+                controller: 'IndexController'
             }
         }
     })
@@ -45,7 +46,8 @@ angular.module('conFusion', ['ionic', 'conFusion.controllers'])
             url: '/menu',
             views: {
                 'mainContent': {
-                    templateUrl: 'templates/menu.html'
+                    templateUrl: 'templates/menu.html',
+                    controller: 'MenuController'
                 }
             }
         })
@@ -71,9 +73,9 @@ angular.module('conFusion', ['ionic', 'conFusion.controllers'])
     .state('app.dishdetails', {
         url: '/menu/:id',
         views: {
-            'menuContent': {
+            'mainContent': {
                 templateUrl: 'templates/dishdetail.html',
-                controller: ''
+                controller: 'DishDetailController'
             }
         }
     });
